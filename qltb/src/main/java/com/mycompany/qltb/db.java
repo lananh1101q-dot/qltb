69fb1465ae151ccdf97e6a5d67ba35c6f78d86fd
lananh1101q-dot <lananh1101q@gmai.com>
2025-12-31 00:50:45 +0700

    trangchu

--- qltb/src/main/java/com/mycompany/qltb/db.java
+++ qltb/src/main/java/com/mycompany/qltb/db.java
@@ -34,7 +34,7 @@ public class db {
     public db() {
         host = "localhost";
         port = "3306";
-        dbName = "kk";
+        dbName = "qltb_thpt";
         url = "jdbc:mysql://" + host + ":" + port + "/" + dbName
                 + "?useSSL=false&serverTimezone=UTC";
         userName = "root";
